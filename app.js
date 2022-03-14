70aae7add311ec14a901047874a2af5984d2a7e4
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-03-14 13:48:30 +0300

    homework 8

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo description',
-  };
-  res.send(photo);
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next(); //next() metodu ile sonraki middleware çağırılabilinir.
+// };
+
+//Template Engine
+app.set('view engine', 'ejs'); //views klasöre bakar
+
+// use the express.static built-in middleware function in Express.
+app.use(express.static('public'));
+// app.use(myLogger);
+
+//Router
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
